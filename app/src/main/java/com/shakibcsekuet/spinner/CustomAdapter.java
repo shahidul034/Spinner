d9dbdf5aca892f082e5bf4f5fe27908632b6dc6f
SHAKIB <shakibkhanrock@gmail.com>
2017-10-31 19:25:22 +0600

    first commit

---
+++ app/src/main/java/com/shakibcsekuet/spinner/CustomAdapter.java
@@ -0,0 +1,28 @@
+package com.shakibcsekuet.spinner;
+
+/**
+ * Created by md sakib on 9/27/2017.
+ */
+
+public class CustomAdapter extends BaseAdapter {
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        return null;
+    }
